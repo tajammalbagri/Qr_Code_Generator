6c4f7749deab974825ab2994154cc8fbca1bc2c0
Tajammal Hussain <50675739+tajammalbagri@users.noreply.github.com>
2020-09-06 22:36:25 +0500

    Update main.py
    final

--- Qr_Code_Generator/main.py
+++ Qr_Code_Generator/main.py
@@ -1,17 +1,34 @@
 from tkinter import*
+from qrcode import*
 
 
 root = Tk()
 root.geometry('390x350')
 root.title('Qr Code Generator')
 
+
 data=StringVar()
+def ShowImg():
+    QrCodeWindow=Toplevel()
+    QrCodeWindow.geometry('400x400')
+    ph=PhotoImage(file='./ico.png')
+    QrCodeWindow.iconphoto(False,ph)
+    QrCodeWindow.resizable(0,0)
+    filename=PhotoImage(file='./QRCODE.PNG')
+    Label(QrCodeWindow,image=filename).pack()
+
+    QrCodeWindow.mainloop()
+
 
 def Click():
-    print(data.get())
+    info=data.get()
+    img=make(info)
+    img.save("./QRCODE.png")
+    ShowImg()
 
 def Clear():
     data.set("")
+
 
 Label(root,text=("Qr Code Generator"),font=('arial',20,'bold')).place(x=60,y=10)
 Label(root,text=("Enter Data "),font=('arial',15,'bold')).place(x=50,y=100)
